c7fdaa030a2f769808944272a82f9ecf55bdaa7e
George Weiler <georgejweiler@gmail.com>
2021-05-06 09:43:47 -0700

    remove web3 react

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
-import { Web3ReactProvider } from '@web3-react/core';
-import { ethers } from 'ethers';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route } from 'react-router-dom';
 import './styles/App.css';
 import Home from './pages/Home';
 import { AppContextProvider } from './AppContext';
 
-function getLibrary(provider) {
-  return new ethers.providers.Web3Provider(provider);
-}
-
 const App = () => {
   return (
     <AppContextProvider>
-      <Web3ReactProvider getLibrary={getLibrary}>
-        <Route exact path="/" component={Home} />
-      </Web3ReactProvider>
+      <Route exact path="/" component={Home} />
     </AppContextProvider>
   );
 };
